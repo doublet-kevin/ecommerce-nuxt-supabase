acc0bb0a2f59f80e78ce5b5300f4cbe619a04cba
kevin-d <kevin.doublet02@gmail.com>
2023-10-05 12:59:18 +0200

    init supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
+  modules: ["@nuxtjs/tailwindcss", '@nuxtjs/supabase'],
+  supabase: {
+    redirectOptions: {
+      login: '/login',
+      callback: '/confirm',
+      exclude: ['*'],
+    }
+  }
 })
